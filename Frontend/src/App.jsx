79e667be657fe806e0ac8faa4ad3272a8a0abdbd
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2024-11-21 23:55:37 +0200

    add home page 🔥

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
-import Header from './component/header'
-import './App.css'
+import { Button, Layout, theme } from 'antd';
+import logo from './assets/logo.png';
+import MenuList from './component/Menulist';
+import THeader from './component/header';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const {Sider, Content } = Layout;
 
-  return (
-    <>
-    <Header />
-    </>
-  )
-}
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
 
-export default App
+    return (
+        <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed} className='sidebar'>
+            <div className="logo" >
+                <img src={logo} alt="Logo" className="logo-img"  />
+            </div>
+            <MenuList />
+        </Sider>
+        <Layout>
+            <THeader collapsed={collapsed} setCollapsed={setCollapsed} />
+            <Content
+            style={{
+                margin: '24px 16px',
+                padding: 24,
+                minHeight: 280,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+            }}
+            >
+            Content
+            </Content>
+        </Layout>
+        </Layout>
+    );
+};
+
+export default App;
