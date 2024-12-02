4f26c0913a2227cdc90896b1ce3671cb4491270f
Nawalhossam <nawalhossam11116@gmail.com>
2024-12-02 22:22:21 +0200

    Edit Menulist💥

--- Frontend/src/component/Menulist.jsx
+++ Frontend/src/component/Menulist.jsx
@@ -1,82 +1,85 @@
 import React from "react";
 import {
-    PayCircleOutlined,
-    SettingOutlined,
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-    DashboardOutlined,
-  } from '@ant-design/icons';
-import {Menu} from 'antd';
-import { CashCoin } from 'react-bootstrap-icons';
-import './Menulist.css';
+  PayCircleOutlined,
+  SettingOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  DashboardOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import { CashCoin } from "react-bootstrap-icons";
+import "./Menulist.css";
 
-const MenuList  =({setCurrentContent}) => {
-    return (
-        <Menu
-        theme="dark"
-        mode="inline"
-        defaultSelectedKeys={['1']}
-        className="menu-list"
-        items={[
+const MenuList = ({ setCurrentContent }) => {
+  return (
+    <Menu
+      theme="dark"
+      mode="inline"
+      defaultSelectedKeys={["1"]}
+      className="menu-list"
+      items={[
+        {
+          key: "1",
+          icon: <DashboardOutlined />,
+          label: "Dashboard",
+          onClick: () => setCurrentContent("1"),
+        },
+        {
+          key: "2",
+          icon: <UserOutlined />,
+          label: "Users Management",
+          children: [
+            { key: "3", label: "Users", onClick: () => setCurrentContent("3") },
             {
-            key: '1',
-            icon: <DashboardOutlined />,
-            label: 'Dashboard',
-            onClick: () => setCurrentContent('1'),
+              key: "4",
+              label: "Approve User",
+              onClick: () => setCurrentContent("4"),
+            },
+          ],
+        },
+        {
+          key: "7",
+          icon: <UploadOutlined />,
+          label: "Stock",
+        },
+        {
+          key: "22",
+          icon: <UserOutlined />,
+          label: "Customer",
+          onClick: () => setCurrentContent("22"),
+        },
+        {
+          key: "8",
+          icon: <CashCoin />,
+          label: "Sales Management",
+          children: [
+            { key: "9", label: "Sales", onClick: () => setCurrentContent("9") },
+            {
+              key: "10",
+              label: "Price Quatation",
+              onClick: () => setCurrentContent("10"),
             },
             {
-            key: '2',
-            icon: <UserOutlined />,
-            label: 'Users Management',
-            children: [
-                { key: '3', label: 'Users' 
-                , onClick: () => setCurrentContent('3')},
-                { key: '4', label: 'Approve User' 
-                , onClick: () => setCurrentContent('4'),
-                },
-              ],
+              key: "11",
+              label: "Debts",
+              onClick: () => setCurrentContent("11"),
             },
-            {
-            key: '7',
-            icon: <UploadOutlined />,
-            label: 'Stock',
-            },
-            {
-              key: '8',
-              icon: <CashCoin />,
-              label: 'Sales Management',
-              children: [
-                  { key: '9', label: 'Sales' 
-                  , onClick: () => setCurrentContent('9')},
-                  { key: '10', label: 'Price Quatation' 
-                  , onClick: () => setCurrentContent('10'),
-                  },
-                  { key: '11', label: 'Debts',
-                    onClick: () => setCurrentContent('11')
-                  },
-                ],
-            },
-            {
-            key: '12',
-            icon: <PayCircleOutlined />,
-            label: 'Purchase Items',
-            onClick: () => setCurrentContent('12'),
-            },
-            {
-            key: '13',
-            icon: <SettingOutlined />,
-            label: 'Setting',
-            },
-            {
-            key: '22',
-            icon: <UserOutlined />,
-            label: 'Customer',
-            onClick: () => setCurrentContent('22'),
-            }
-        ]}
-        />
-    )
-
-}
-export default MenuList
+          ],
+        },
+        {
+          key: "12",
+          icon: <PayCircleOutlined />,
+          label: "Purchase Items",
+          onClick: () => setCurrentContent("12"),
+        },
+        {
+          key: "13",
+          icon: <SettingOutlined />,
+          label: "Setting",
+        },
+      ]}
+    />
+  );
+};
+export default MenuList;
